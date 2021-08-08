5b9d2fd5cd6424405e170205ab051015e028720a
Mohammad <88484934+trabit373@users.noreply.github.com>
2021-08-08 19:27:10 +0430

    Create main_network.py

---
+++ main_network.py
@@ -0,0 +1,48 @@
+def _all_():
+    import os
+    import time
+    import sys
+    try:
+        from colorama import Fore
+    except:
+        time.sleep(1)
+        print("[-] Pleass Install The Librery --> colorama")
+        sys.exit()
+    from baner import baner
+    os.system("clear")
+    baner()
+    time.sleep(1)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "1" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Traceroute Using MTR")
+    time.sleep(0.3)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "2" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Test Ping")
+    time.sleep(0.3)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "00" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Back")
+    time.sleep(0.3)
+    #######################  Get Number #######################
+    try:
+        number = int(input(Fore.WHITE + "\n  Enter Your Number " + Fore.YELLOW + ">>>" + Fore.BLUE + " "))
+    except:
+        time.sleep(0.8)
+        print(Fore.RED + "\n[-] Your Input Is Not Number !")
+        time.sleep(0.5)
+        sys.exit()
+    else:
+        if number == 1:
+            import mtr
+            mtr._all_()
+            # در اینجا کدی میزنیم که به بخش قبل برود
+
+        elif number == 2:
+            import nping
+            nping._all_()
+            # در اینجا کدی میزنیم که به بخش قبل برود
+
+        elif number == 00:
+            pass
+            # در اینجا کدی میزنیم که به بخش قبل برود
+        
+        else:
+            time.sleep(0.4)
+            print(Fore.RED + "\n[-] Your Number Is Not Found !")
+            time.sleep(0.4)
+            sys.exit()
