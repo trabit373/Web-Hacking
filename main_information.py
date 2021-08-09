dc3252715104ced12c25e4cd4ffebe4a30737ff8
Mohammad <88484934+trabit373@users.noreply.github.com>
2021-08-09 09:44:56 +0430

    Create main_information.py

---
+++ main_information.py
@@ -0,0 +1,49 @@
+def _all_():
+    import os
+    import time
+    import sys
+    try:
+        from colorama import Fore
+    except:
+        time.sleep(1)
+        print("[-] Pleass Install The Librery --> colorama")
+        sys.exit()
+    from baner import baner
+    os.system("clear")
+    baner()
+    time.sleep(1)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "1" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Information Data For Librery Builtwith")
+    time.sleep(0.3)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "2" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Active Site On Server")
+    time.sleep(0.3)
+    print(Fore.YELLOW + "\t\t\t\t\t  [" + Fore.GREEN + "00" + Fore.YELLOW + "]" + Fore.BLUE + " ~ " + Fore.CYAN + "Back")
+    time.sleep(0.3)
+    #######################  Get Number #######################
+    try:
+        number = int(input(Fore.WHITE + "\n  Enter Your Number " + Fore.YELLOW + ">>>" + Fore.BLUE + " "))
+    except:
+        time.sleep(0.8)
+        print(Fore.RED + "\n[-] Your Input Is Not Number !")
+        time.sleep(0.5)
+        sys.exit()
+    else:
+        if number == 1:
+            import builtwith
+            builtwith._all_()
+            # در اینجا کدی میزنیم که به بخش قبل برود
+
+        elif number == 2:
+            import site_on_server
+            site_on_server._all_()
+            # در اینجا کدی میزنیم که به بخش قبل برود
+
+
+        elif number == 00:
+            pass
+            # در اینجا کدی میزنیم که به بخش قبل برود
+        
+        else:
+            time.sleep(0.4)
+            print(Fore.RED + "\n[-] Your Number Is Not Found !")
+            time.sleep(0.4)
+            sys.exit()
